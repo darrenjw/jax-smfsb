0b11baaa6e4b958ac16e5b2838b173150d1177e4
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2024-04-19 18:40:13 +0100

    a first working test

--- tests/test_spn.py
+++ tests/test_spn.py
@@ -1,7 +1,18 @@
 # test_spn.py
 
+import jsmfsb
+import jax
 
-print("hello")
+
+
+def test_spn():
+    lv = jsmfsb.models.lv()
+    step = lv.stepGillespie()
+    k0 = jax.random.key(42)
+    x1 = step(k0, lv.m, 0, 1)
+    assert(x1.shape == (2,))
+
+
 
 
 
