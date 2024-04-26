c9c4887ed528887394f2280be656da68e6221782
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2024-04-26 17:13:18 +0100

    demos for the built-in models

--- demos/lv.py
+++ demos/lv.py
@@ -7,6 +7,7 @@ import jax.numpy as jnp
 import jax.scipy as jsp
 import jax.lax as jl
 from jax import grad, jit
+import matplotlib.pyplot as plt
 
 import jsmfsb
 
@@ -18,19 +19,6 @@ print(step(k0, lvmod.m, 0, 30))
 stepC = lvmod.stepCLE(0.01)
 print(stepC(k0, lvmod.m, 0, 30))
 
-
-# simTs
-out = jsmfsb.simTs(k0, lvmod.m, 0, 30, 0.1, step)
-
-import matplotlib.pyplot as plt
-fig, axis = plt.subplots()
-for i in range(2):
-	axis.plot(range(out.shape[0]), out[:,i])
-
-axis.legend(lvmod.n)
-fig.savefig("lv.pdf")
-
-# simSample
 out = jsmfsb.simSample(k0, 10000, lvmod.m, 0, 30, stepC)
 out = jnp.where(out > 1000, 1000, out)
 import scipy as sp
@@ -38,7 +26,7 @@ print(sp.stats.describe(out))
 fig, axes = plt.subplots(2,1)
 for i in range(2):
     axes[i].hist(out[:,i], bins=50)
-fig.savefig("lvH.pdf")
+fig.savefig("lv.pdf")
 
 # eof
 
