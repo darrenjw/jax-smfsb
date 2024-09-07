e6eb2cfabf15f410eefdb582882e96438672db64
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2024-09-07 21:46:12 +0100

    calibrated abc working

--- demos/abc-cal.py
+++ demos/abc-cal.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
-# abc.py
-# ABC using Euclidean distance (no summary stats)
+# abc-cal.py
+# ABC with calibrated summary stats
 
 import jsmfsb
 import jax
 import jax.numpy as jnp
 import jax.scipy as jsp
+#import math
 
-print("ABC")
+print("ABC with calibrated summary stats")
 
 data = jsmfsb.data.LVperfect[:,1:3]
 
@@ -21,8 +22,41 @@ def rmod(k, th):
   return jsmfsb.simTs(k, jnp.array([50.0, 100.0]), 0, 30, 2,
                      jsmfsb.models.lv(th).stepCLE(0.1))
 
+def ss1d(vec):
+  n = len(vec)
+  mean = jnp.nanmean(vec)
+  v0 = vec - mean
+  var = jnp.nanvar(v0)
+  acs = [jnp.corrcoef(v0[0:(n-1)], v0[1:n])[0,1],
+         jnp.corrcoef(v0[0:(n-2)], v0[2:n])[0,1],
+         jnp.corrcoef(v0[0:(n-3)], v0[3:n])[0,1]]
+  #print(mean)
+  #print(var)
+  #print(acs)
+  return jnp.array([jnp.log(mean+1), jnp.log(var+1), acs[0], acs[1], acs[2]])
+
+def ssi(ts):
+  return jnp.concatenate((ss1d(ts[:,0]), ss1d(ts[:,1]),
+                          jnp.array([jnp.corrcoef(ts[:,0], ts[:,1])[0,1]])))
+
+print("Pilot run")
+k0 = jax.random.key(42)
+k1, k2 = jax.random.split(k0)
+
+p, d = jsmfsb.abcRun(k1, 100000, rpr, lambda k,th: ssi(rmod(k,th)), batch_size=10000)
+prmat = jnp.vstack(p)
+dmat = jnp.vstack(d)
+print(prmat.shape)
+print(dmat.shape)
+dmat = dmat.at[dmat == jnp.inf].set(jnp.nan)
+sds = jnp.nanstd(dmat, 0)
+print(sds)
+
+
+print("Main run with calibrated summary stats")
+
 def sumStats(dat):
-  return dat
+  return ssi(dat)/sds
 
 ssd = sumStats(data)
 
@@ -33,8 +67,7 @@ def dist(ss):
 def rdis(k, th):
   return dist(sumStats(rmod(k, th)))
 
-k0 = jax.random.key(42)
-p, d = jsmfsb.abcRun(k0, 1000000, rpr, rdis, batch_size=100000, verb=False)
+p, d = jsmfsb.abcRun(k2, 1000000, rpr, rdis, batch_size=100000, verb=False)
 
 q = jnp.nanquantile(d, 0.01)
 prmat = jnp.vstack(p)
@@ -52,7 +85,7 @@ axes[0, 2].scatter(postmat[:,1], postmat[:,2], s=0.5)
 axes[1, 0].hist(postmat[:,0], bins=30)
 axes[1, 1].hist(postmat[:,1], bins=30)
 axes[1, 2].hist(postmat[:,2], bins=30)
-fig.savefig("abc.pdf")
+fig.savefig("abc-cal.pdf")
 
 print("All done.")
 
